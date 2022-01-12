68410a0012fc890d26f13918596ddba60f83e2d1
Rico Zhu <ricozhuthegreat@hotmail.com>
2022-01-11 20:40:02 -0500

    Added Additional Attributes to DataModule

--- utils.py
+++ utils.py
@@ -9,16 +9,25 @@ from torch.utils.data import DataLoader, Dataset
 
 class DataModule():
 
-    def __init__(self, data_dir, resize=(60, 60), max_data=None, n_test_simulation=12, batch_size=50):
+    def __init__(self, data_dir, resize=(60, 60), max_data=None, n_test_simulation=12, batch_size=50,
+                    skip_steps=10, store_steps_ahead=5, test_ratio=0.2):
 
         # Set class attributes
         self.data_dir = data_dir
+
+        # Data constraints
         self.resize = resize
         self.max_data = max_data
         self.n_test_simulations = n_test_simulation
         self.batch_size = batch_size
 
+        self.skip_steps = skip_steps
+        self.store_steps_ahead = store_steps_ahead
+
+        # Load all data
         self.names, self.data_arrays = self.load_data()
+        self.n_train = int((1 - self.test_ratio) * len(self.data_arrays))
+
 
     def load_data(self):
 
@@ -36,3 +45,30 @@ class DataModule():
                     (len(array), 1, self.resize[0], self.resize[1]))
 
         return data_files, data_arrays
+
+    def prepare_x_y(self, simulations , skip_steps = 10, store_steps_ahead = 5):
+
+        X = []
+        Y = []
+
+        for simulation in tqdm(simulations):
+            sim = simulation[2:] # Remove first spiky snapshots
+            lsim = len(sim)
+
+            for i in range(int(np.floor(lsim/skip_steps)) - store_steps_ahead):
+                s = i * skip_steps
+                _Y = []
+
+                for j in range(1,store_steps_ahead):
+                    sj = (i + j) * skip_steps
+                    _Y.append(sim[sj])
+
+                _Y = np.array(_Y)
+
+                X.append(sim[s])
+                Y.append(_Y)
+
+        X = np.array(X)
+        Y = np.array(Y)
+
+        return X, Y
