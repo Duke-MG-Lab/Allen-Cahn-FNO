Fixed Loss Calculation

main.py
@@ -34,21 +34,19 @@ def train(input_config, output_config, model_config):
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
     l1_loss = torch.nn.L1Loss()
 
-    for i in range(mc["max_epochs"]):
+    for epoch in range(mc["max_epochs"]):
 
-        train_l2_step = 0
-        train_l2_full = 0
+        epoch_training_loss = []
+        epoch_validation_loss = []
 
         # Training
-        for index, batch in enumerate(data_module.train_dataloader):
+        for i, batch in enumerate(data_module.train_dataloader):
 
-            Xb, Ystep1, Ystep2, Ystep3, Ystep4 = batch["X"],batch["Y"][:,0,:,:,:],batch["Y"][:,1,:,:,:],batch["Y"][:,2,:,:,:],batch["Y"][:,3,:,:,:]
+            Xb, Ystep1, Ystep2, Ystep3, Ystep4 = batch["X"], batch["Y"][:,0,:,:,:], batch["Y"][:,1,:,:,:], batch["Y"][:,2,:,:,:], batch["Y"][:,3,:,:,:]
             Ydata = [Ystep1, Ystep2, Ystep3, Ystep4]
 
             Ypred1 = model(Xb)
-
             loss1 = l1_loss(Ypred1, Ystep1)
-
             Ypred = Ypred1
 
             losses = []
@@ -62,18 +60,24 @@ def train(input_config, output_config, model_config):
             losses = [l.view(1) for l in losses]
             loss = torch.mean(torch.cat(losses, 0))
 
+            epoch_training_loss.append(loss)
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
+        # Update learning rate with scheduler and output epoch training loss
         scheduler.step()
+        mean_training_loss = torch.mean(torch.Tensor(epoch_training_loss))
+
+        print("epoch: ", epoch, "training loss: ", mean_training_loss)
 
         # Validation
         with torch.no_grad():
 
-            for index, batch in enumerate(data_module.test_dataloader):
+            for i, batch in enumerate(data_module.test_dataloader):
 
-                Xb, Ystep1, Ystep2, Ystep3, Ystep4 = batch["X"],batch["Y"][:,0,:,:,:],batch["Y"][:,1,:,:,:],batch["Y"][:,2,:,:,:],batch["Y"][:,2,:,:,:]
+                Xb, Ystep1, Ystep2, Ystep3, Ystep4 = batch["X"], batch["Y"][:,0,:,:,:], batch["Y"][:,1,:,:,:], batch["Y"][:,2,:,:,:], batch["Y"][:,2,:,:,:]
                 Ypred1 = model(Xb)
                 Ypred2 = model(Ypred1)
                 Ypred3 = model(Ypred2)
@@ -83,3 +87,8 @@ def train(input_config, output_config, model_config):
                 val_loss2 = l1_loss(Ypred2, Ystep2)
                 val_loss3 = l1_loss(Ypred3, Ystep3)
                 val_loss4 = l1_loss(Ypred4, Ystep4)
+
+                epoch_validation_loss.append([val_loss1, val_loss2, val_loss3, val_loss4])
+
+        validation_step_outputs = np.array(torch.mean(torch.Tensor(epoch_validation_loss), axis =0))
+        print("epoch: ", epoch, "validation loss: ", validation_step_outputs[0])